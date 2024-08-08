770c4d4b396a849f08976b1080890359223e94ac
Jo-M04 <joseph.i.miller004@gmail.com>
2024-08-08 15:33:28 -0500

    had to maker a new repo

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logoutButton = document.querySelector('#logout');
+
+if (logoutButton) {
+  logoutButton.addEventListener('click', async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  });
+}
